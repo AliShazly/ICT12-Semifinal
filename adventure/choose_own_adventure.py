4da291e81a680fccfe40da1bfb68f052cb26a1b4
Ali El-Shazly <040710s@sd44.ca>
2019-04-18 11:15:22 -0700

    Add gameover and backtracking

--- adventure/choose_own_adventure.py
+++ adventure/choose_own_adventure.py
@@ -4,20 +4,27 @@ import io
 import sys
 
 class Story:
-    def __init__(self, desc, choices, next_scenes, art=None, height=0, width=0):
-        self.desc = desc
-        self.choices = choices
-        self.art = art
+    # def __init__(self, desc, choices, next_scenes=None, art=None, height=0, width=0):
+    #     self.desc = desc
+    #     self.choices = choices
+    #     self.art = art
+    #     self.next_scenes = next_scenes
+    #     self.height = height
+    #     self.width = width
+
+    def __init__(self, story_dict, height=0, width=0):
+        self.desc = story_dict['desc']
+        self.choices = story_dict['choices']
+        self.art = story_dict['art']
+        self.next_scenes = story_dict
         self.height = height
         self.width = width
-        self.next_scenes = next_scenes
-
 
     def _init_terminal(self, stdscr):
         stdscr.clear()
         stdscr.refresh()
 
-        min_h, min_w = 40, 55
+        min_h, min_w = 50, 100
         term_height, term_width = stdscr.getmaxyx()
 
         if term_height < min_h:
@@ -30,7 +37,7 @@ class Story:
         self.height, self.width = term_height, term_width
 
 
-    def _draw_description(self, stdscr, desc):
+    def _draw_description(self, stdscr, desc, center=False):
         y_pos = 0
         desc_list = desc.split(' ')
 
@@ -39,8 +46,14 @@ class Story:
             if y_pos >= self.width:
                 desc_list[i] = f'\n{desc_list[i]}'
                 y_pos = 0
-    
-        stdscr.addstr(0, 0, ' '.join(desc_list))
+        msg = ' '.join(desc_list)
+
+        if not center:
+            stdscr.addstr(0, 0, msg)
+        else:
+            start_x, start_y = 0, self.height//2 # TODO: Center x val
+            print(start_x, start_y, len(msg), msg)
+            stdscr.addstr(start_y, start_x, msg)
 
 
     def _draw_border(self, stdscr):
@@ -75,11 +88,21 @@ class Story:
         stdscr.addstr(start_y_art, start_x_art, art_formatted)
 
 
-    def draw_scene(self, stdscr, k=1):
+    def _draw_gameover(self, stdscr, msg='Game over.'):
+        self._init_terminal(stdscr)
+        self._draw_art(stdscr, open('gameover.txt').read())
+        msg_x, msg_y = self.width//2 - len(msg)//2, self.height - self.height//4
+        stdscr.addstr(msg_y, msg_x, msg)
+        stdscr.refresh()
+        stdscr.getch()
+
+    def _draw_scene(self, stdscr, k=1):
         self._init_terminal(stdscr)
         if self.art:
             self._draw_art(stdscr, self.art)
-        self._draw_description(stdscr, self.desc)
+            self._draw_description(stdscr, self.desc)
+        else:
+            self._draw_description(stdscr, self.desc, center=True)
         self._draw_border(stdscr)
         self._draw_choices(stdscr, self.choices)
 
@@ -92,23 +115,36 @@ class Story:
         if chr(k) in (str(i) for i in range(1, len(self.choices)+1)):
             return chr(k)
 
-        self.draw_scene(stdscr, k)
+        self._draw_scene(stdscr, k)
 
 
-    def draw_story(self, stdscr, depth=1): # Remove depth var, pass dict into class instead of dict.values
-        key = self.draw_scene(stdscr)
-        if depth == 1:
-            self.next_scenes = self.next_scenes[str(key)]
-        else:
-            self.next_scenes = self.next_scenes["next_scenes"][str(key)]
+    def draw_story(self, stdscr, previous_scene=None):
+        key = self._draw_scene(stdscr)
+
+        try:
+            if 'gameover' in self.next_scenes['next_scenes'][str(key)]:
+                msg = self.next_scenes['next_scenes'][str(key)]['gameover']
+                self._draw_gameover(stdscr, msg)
+                return
+        except KeyError:
+            pass
+
+        try:
+            if self.next_scenes['next_scenes'][str(key)]['choices'] == ['Go back']:
+                previous_scene = self.next_scenes
+            self.next_scenes = self.next_scenes['next_scenes'][str(key)]
+        except KeyError:
+            self.next_scenes = previous_scene
+        
         self.desc = self.next_scenes['desc']
         self.choices = self.next_scenes['choices']
         self.art = self.next_scenes['art']
-        return self.draw_story(stdscr, depth+1)
+        return self.draw_story(stdscr, previous_scene)
+
 
 with io.open('paths.json', 'r', encoding='utf-8-sig') as infile:
     amnesia_dict = json.load(infile)
 
 
-amnesia = Story(*amnesia_dict.values())
+amnesia = Story(amnesia_dict)
 curses.wrapper(amnesia.draw_story)
