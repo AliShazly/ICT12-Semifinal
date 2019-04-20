170203916057d80f8037b961de2fa4371c09a15c
AliShazly <alishazly302@gmail.com>
2019-04-19 17:29:16 -0700

    Finished basic back-and forth framework

--- adventure/choose_own_adventure.py
+++ adventure/choose_own_adventure.py
@@ -1,16 +1,8 @@
 import curses
 import json
 import io
-import sys
 
 class Story:
-    # def __init__(self, desc, choices, next_scenes=None, art=None, height=0, width=0):
-    #     self.desc = desc
-    #     self.choices = choices
-    #     self.art = art
-    #     self.next_scenes = next_scenes
-    #     self.height = height
-    #     self.width = width
 
     def __init__(self, story_dict, height=0, width=0):
         self.desc = story_dict['desc']
@@ -37,6 +29,12 @@ class Story:
         self.height, self.width = term_height, term_width
 
 
+    def _center_width(self, text):
+        line_len = len(text.split('\n')[0])
+        center = self.width // 2 - line_len // 2
+        return (abs(center)+center)//2 # Turning negative numbers to 0
+
+
     def _draw_description(self, stdscr, desc, center=False):
         y_pos = 0
         desc_list = desc.split(' ')
@@ -51,9 +49,11 @@ class Story:
         if not center:
             stdscr.addstr(0, 0, msg)
         else:
-            start_x, start_y = 0, self.height//2 # TODO: Center x val
-            print(start_x, start_y, len(msg), msg)
-            stdscr.addstr(start_y, start_x, msg)
+            start_y = self.height // 2
+            for i in msg.split('\n'):
+                start_x = self._center_width(i)
+                stdscr.addstr(start_y, start_x, i)
+                start_y += 2
 
 
     def _draw_border(self, stdscr):
@@ -65,21 +65,14 @@ class Story:
         start_y_choices = self.height//2 + self.height//4
         option_num = 1
         for i, j in zip(range(0, len(choices)*2, 2), choices):
-            x = self.width//2 - len(j)//2
+            x = self._center_width(j)
             stdscr.addstr(start_y_choices + i, x, f'{option_num}: {j}')
             option_num += 1
 
 
     def _draw_art(self, stdscr, art):
         start_y_art = self.height//2 - self.height//3 - 4
-
-        line_len = 0
-        for i in art:
-            if i == '\n':
-                break
-            line_len +=1
-        
-        start_x_art = self.width//2 - line_len//2
+        start_x_art = self._center_width(art)
         count = sorted([(i, art.count(i)) for i in set(art)], key=lambda x: x[1], reverse=True)
         background_char = count[0][0]
         art_formatted = art.replace(background_char, ' ')
@@ -88,13 +81,30 @@ class Story:
         stdscr.addstr(start_y_art, start_x_art, art_formatted)
 
 
-    def _draw_gameover(self, stdscr, msg='Game over.'):
+    def _draw_endscreen(self, stdscr, msg, gameover):
         self._init_terminal(stdscr)
-        self._draw_art(stdscr, open('gameover.txt').read())
-        msg_x, msg_y = self.width//2 - len(msg)//2, self.height - self.height//4
+        with io.open('endscreens.json', 'r', encoding='utf-8-sig') as infile:
+            endscren = json.load(infile)
+        art = endscren['gameover'] if gameover else endscren['gamewin']
+        self._draw_art(stdscr, art)
+        msg_x = self._center_width(msg)
+        msg_y = self.height - self.height // 4
         stdscr.addstr(msg_y, msg_x, msg)
         stdscr.refresh()
         stdscr.getch()
+
+    
+    def _draw_input(self, stdscr, msg):
+        self._init_terminal(stdscr)
+        curses.echo()
+        msg_x = self._center_width(msg)
+        msg_y = self.height - self.height // 4
+        stdscr.addstr(msg_y, msg_x, msg)
+        stdscr.refresh()
+        inp = stdscr.getstr(msg_y, msg_x + len(msg))
+        curses.noecho()
+        return inp.decode('utf-8')
+
 
     def _draw_scene(self, stdscr, k=1):
         self._init_terminal(stdscr)
@@ -106,31 +116,41 @@ class Story:
         self._draw_border(stdscr)
         self._draw_choices(stdscr, self.choices)
 
-        if k == ord('q') or k == 27: # ESC
-            raise SystemExit
-
         stdscr.refresh()
-        k = stdscr.getch()
 
-        if chr(k) in (str(i) for i in range(1, len(self.choices)+1)):
-            return chr(k)
+        while True:
+            k = stdscr.getch()
+            if chr(k) in (str(i) for i in range(1, len(self.choices)+1)):
+                return chr(k)
 
-        self._draw_scene(stdscr, k)
+            if k == ord('q') or k == 27: # ESC
+                raise SystemExit
 
 
     def draw_story(self, stdscr, previous_scene=None):
-        key = self._draw_scene(stdscr)
 
+        # def log(msg):
+        #     with open('log.txt', 'a+') as f:
+        #         f.write(str(msg) + '\n')
+
+        key = self._draw_scene(stdscr)
         try:
-            if 'gameover' in self.next_scenes['next_scenes'][str(key)]:
-                msg = self.next_scenes['next_scenes'][str(key)]['gameover']
-                self._draw_gameover(stdscr, msg)
-                return
+            next_scene = self.next_scenes['next_scenes'][str(key)]
+
+            if 'gameover' in next_scene:
+                msg = next_scene['gameover']
+                return self._draw_endscreen(stdscr, msg, gameover=True)
+                
+            elif 'gamewin' in next_scene:
+                msg = next_scene['gamewin']
+                return self._draw_endscreen(stdscr, msg, gameover=False)
+
+                
         except KeyError:
             pass
 
         try:
-            if self.next_scenes['next_scenes'][str(key)]['choices'] == ['Go back']:
+            if 'Go back' in self.next_scenes['next_scenes'][str(key)]['choices']:
                 previous_scene = self.next_scenes
             self.next_scenes = self.next_scenes['next_scenes'][str(key)]
         except KeyError:
