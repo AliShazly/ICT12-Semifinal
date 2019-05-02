3fcf7e0a130784af124451ca9e41dbb8aaf5d1c1
Ali El-Shazly <040710s@sd44.ca>
2019-05-02 11:15:11 -0700

    Updated window features

--- login/login.py
+++ login/login.py
@@ -1,17 +1,15 @@
 import curses
 
 class Window:
-    def __init__(self, options, height=0, width=0):
-        self.options = options
-        self.height = height
-        self.width = width
+    def __init__(self, stdscr):
+        self.stdscr = stdscr
 
-    def init_screen(self, stdscr):
-        stdscr.clear()
-        stdscr.refresh()
+    def _init_screen(self):
+        self.stdscr.clear()
+        self.stdscr.refresh()
 
         min_h, min_w = 50, 100
-        term_height, term_width = stdscr.getmaxyx()
+        term_height, term_width = self.stdscr.getmaxyx()
 
         if term_height < min_h:
             curses.resize_term(min_h, term_width)
@@ -21,22 +19,40 @@ class Window:
             term_width = min_w
 
         self.height, self.width = term_height, term_width
-    
+
 
     def _center_width(self, text):
         line_len = len(text.split('\n')[0])
         center = self.width // 2 - line_len // 2
-        return (abs(center)+center)//2 # Turning negative numbers to 0
+        return (abs(center) + center) // 2 # Turning negative numbers to 0
 
 
-    def draw_options(self, stdscr, separation=5):
+    def draw_options(self, options, separation=5):
+        self._init_screen()
         start_y_choices = self.height//2
-        y_step = range(0, len(self.options) * separation, separation)
-        option_nums = range(1, len(self.options) + 1)
+        y_step = range(0, len(options) * separation, separation)
+        option_nums = range(1, len(options) + 1)
 
-        for step, option, num in zip(y_step, self.options, option_nums):
+        for step, option, num in zip(y_step, options, option_nums):
             x_val = self._center_width(option)
-            stdscr.addstr(start_y_choices + step, x_val, f'{num}: {option}')
+            self.stdscr.addstr(start_y_choices + step, x_val, f'{num}: {option}')
+        while True:
+            choice = chr(self.stdscr.getch())
+            if choice in (str(i) for i in range(1, len(options) + 1)):
+                return choice
+
+
+    def draw_input(self, msg):
+        self._init_screen()
+        curses.echo()
+        msg_x = self._center_width(msg)
+        msg_y = self.height // 2
+        self.stdscr.addstr(msg_y, msg_x, msg)
+        self.stdscr.refresh()
+        inp = self.stdscr.getstr(msg_y, msg_x + len(msg))
+        curses.noecho()
+        return inp.decode('utf-8')
+
 
 
 class Login(Window):
@@ -49,6 +65,9 @@ class CreateAcc(Window):
         pass
 
 
+def main(stdscr):
+    test = Window(stdscr)
+    test.draw_options(['Test1','Test2','Test3','Test4'])
 
-test = Window(['Login', 'Create an Account'])
-curses.wrapper(test.draw_options)
+
+curses.wrapper(main)
